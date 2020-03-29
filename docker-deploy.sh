e288fb74a304fda1b5f86de9cbc5b7e0052c93ac
hyojaekim <hj00686@naver.com>
2020-03-29 23:40:25 +0900

    Travis CI Test

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,10 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-docker build --tag woowacrew/woowacrew:0.0.1 .
+docker build --tag woowacrew/woowacrew-ci:0.0.1 .
 
-docker tag woowacrew/woowacrew:0.0.1 woowacrew/woowacrew:latest
+docker tag woowacrew/woowacrew-ci:0.0.1 woowacrew/woowacrew-ci:latest
 
-docker push woowacrew/woowacrew:0.0.1
-docker push woowacrew/woowacrew:latest
+docker push woowacrew/woowacrew-ci:0.0.1
+docker push woowacrew/woowacrew-ci:latest
 
 echo "정상적인 실행"
